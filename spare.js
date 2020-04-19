1b04a1fbec6e550465cd7fa07c707507e8c0395c
jimbobding <jimmygrahamdavid@gmail.com>
2020-04-19 21:35:15 +0000

    initial commit

---
+++ spare.js
@@ -0,0 +1,7 @@
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 53.4808, lng: -2.2426 },
+    zoom: 13,
+    mapTypeId: 'roadmap',
+  });
+}
